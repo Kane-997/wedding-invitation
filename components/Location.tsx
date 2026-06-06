4775adcb355d862cac23bb62517c8cf8692cba17
Kane-997 <nhathaokh@gmail.com>
2026-06-06 14:30:47 +0700

    Updated Location.tsx

--- components/Location.tsx
+++ components/Location.tsx
@@ -10,7 +10,7 @@ const venues = [
     time: '17h30',
     date: 'Chủ Nhật, 19/07/2026',
     mapUrl:
-      'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d5121.061892682316!2d109.21558885130312!3d13.787749262049624!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x316f6b62562a5253%3A0x7262732c53d33395!2sS%C3%A1u%20Cao%202%20Restaurant!5e1!3m2!1svi!2s!4v1780730720990!5m2!1svi!2s,
+      'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d5121.061892682316!2d109.21558885130312!3d13.787749262049624!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x316f6b62562a5253%3A0x7262732c53d33395!2sS%C3%A1u%20Cao%202%20Restaurant!5e1!3m2!1svi!2s!4v1780730720990!5m2!1svi!2s'
     directionsUrl: 'https://maps.app.goo.gl/6T2GQN324Mk5oQCa6',
   },
   {
