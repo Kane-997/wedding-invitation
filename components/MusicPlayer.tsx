53cc5652a7845d821f9f5f43fbea8e21d586f0e9
Kane-997 <nhathaokh@gmail.com>
2026-06-07 21:13:31 +0700

    Updated MusicPlayer.tsx

--- components/MusicPlayer.tsx
+++ components/MusicPlayer.tsx
@@ -8,7 +8,7 @@ export default function MusicPlayer() {
   const [playing, setPlaying] = useState(false);
   const [started, setStarted] = useState(false);
 
-  const MUSIC_URL = 'https://cdn.pixabay.com/download/audio/2022/02/15/audio_d3c4eccd51.mp3';
+  const MUSIC_URL = '/audio/Mot-Doi.mp3';
 
   const toggle = () => {
     const audio = audioRef.current;
