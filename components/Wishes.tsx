eccec76931d62404181dc0ce1c40e894d2bc3e70
Kane-997 <nhathaokh@gmail.com>
2026-06-20 09:31:14 +0700

    Updated Wishes.tsx

--- components/Wishes.tsx
+++ components/Wishes.tsx
@@ -25,7 +25,7 @@ export default function Wishes() {
           .not('message', 'is', null)
           .neq('message', '')
           .order('created_at', { ascending: false })
-          .limit(7);
+          .limit(2);
 
         if (data) {
           setWishes(
